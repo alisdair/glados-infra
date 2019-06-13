67ee67426be3eb4af8eadae735ecf4b26137c8e7
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2019-06-12 21:21:54 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "name" {
+  type = "string"
+  default = "Subject Name Here"
+}
+variable "hometown" {
+  type = "string"
+  default = "Subject Hometown Here"
+}
+
+locals {
+  greeting = "Unbelievable! You, ${var.name}, must be the pride of ${var.hometown}."
+}
+
+output "greeting" {
+  value = "${local.greeting}"
+}
+
+resource "null_resource" "result" {
+  triggers = {
+    greeting = "${local.greeting}"
+  }
+}
